55045d336b62937e2493671f1af22693ba0b3553
unknown <8574aman@gmail.com>
2021-05-08 16:51:57 +0530

    added dependecies for sleuth_framework_to_generate_unique_id and zipkin to connect MS to zipkin server

--- currency-exchange-service/src/main/java/com/currency/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/currency/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.currency.microservices.currencyexchangeservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,13 @@ private Environment envoironment;
 @Autowired
 private CurrencyExchangeRespository CurrencyExchangeRespository;
 
+private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 @GetMapping("/currency-exchange/from/{from}/to/{to}")
 public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to)
 {
+	logger.info("GET currency exchange from {} to {}",from,to);
+	
 	CurrencyExchange currencyExchange = CurrencyExchangeRespository.findByFromAndTo(from, to);
 
 	if(currencyExchange == null)
